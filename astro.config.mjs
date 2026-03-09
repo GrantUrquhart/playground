a09b42cab704a8b1c34862e64e0e1e76346afaaf
Grant Urquhart <grantu@Grants-Mac-mini.local>
2026-03-09 01:10:45 -0500

    finished astro demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://iridescent-zabaione-7a469e.netlify.app/blog/",
+  integrations: [preact()]
+});
