27d78d03aed84a429e80f683ffadf568dddbae84
GrantUrquhart <granturquhart3@gmail.com>
2026-05-09 20:16:03 -0500

    removed comments

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,8 @@
-// Import the glob loader
+
 import { glob } from "astro/loaders";
-// Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
-// Import Zod
 import { z } from "astro/zod";
-// Define a `loader` and `schema` for each collection
+
 const blog = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
     schema: z.object({
